e8208834950e43e20974a3e81a63e4ee9192bec0
Ahmed Sabbour <sabbour@outlook.com>
2017-09-12 16:46:36 +0400

    CICD AppService: Work in progress.

---
+++ ApplicationModernization/CICDAppServiceVSTS/src/colormicroservice/routes/colors.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var os = require('os');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var computerName = os.hostname();
+
+  //var randomColor = getRandomGrayScaleColor();
+  var randomColor = getRandomColor();
+
+  var json = JSON.stringify({ 
+    computerName: computerName, 
+    color: randomColor
+  });
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.end(json);
+});
+
+function getRandomColor() {
+  var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});  
+  return randomColor;  
+}
+
+function getRandomGrayScaleColor() {
+  var value = Math.random() * 0xFF | 0;
+  var grayscale = (value << 16) | (value << 8) | value;
+  var randomColor = '#' + grayscale.toString(16);
+  return randomColor;
+}
+
+module.exports = router;
